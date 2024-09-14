fc430470bbf46adb65b1374a9223b499fda88196
YotoGarcia999 <jonathan.garciaab@est.iudigital.edu.co>
2024-09-14 18:39:13 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Header } from './components/ui/Header'
+import { DirectorView } from './components/director/DirectorView';
+import { GeneroView } from './components/genero/GeneroView';
+import { MediaView } from './components/media/MediaView';
+import { ProdcutoraView } from './components/productora/ProdcutoraView';
+import { TipoView } from './components/tipo/TipoView';
+
 
 const App = () => {
-  return (<h1>Hola mundo</h1>)
+  return (<Header/>)
 }
 
 export {
